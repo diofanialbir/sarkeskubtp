3597f6a30c5f6bb231ba308eb9bd5f1bded266a1
Diofani <diofanialbir@gmail.com>
2025-12-16 10:34:52 +0700

    ubah nama

--- script.js
+++ script.js
@@ -36,6 +36,6 @@ function startWebsite() {
     document.body.style.opacity = '0';
 
     setTimeout(() => {
-        window.location.href = 'main-pembagian-tugas.html';   // pindah ke halaman utama
+        window.location.href = 'main-index.html';   // pindah ke halaman utama
     }, 800);
 }
